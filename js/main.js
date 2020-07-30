606ad5f9c10ffa1a1517377ddeaa0c573a7fe811
archie-1st <rustamjon_yusupov_99@inbox.ru>
2020-07-30 18:50:38 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(function(){
 
-    $('.slider_inner').slick();
-
+    $('.slider_inner').slick({
+        nextArrow: 
+        '<button type="button" class="slick-btn slick-next"></button>})',
+        prevArrow: 
+        '<button type="button" class="slick-btn slick-prev"></button>})',
 
 }); 
+
+
+ 
+
+
+});    
